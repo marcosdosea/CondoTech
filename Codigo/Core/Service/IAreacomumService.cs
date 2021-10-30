527187ef2322af53d1a1dd6369df5e91dfbf18d1
raul152 <48693258+raul152@users.noreply.github.com>
2021-10-29 23:43:02 -0300

    Referente a issue #62

---
+++ Codigo/Core/Service/IAreacomumService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAreacomumService
+    {
+        int Inserir(Areacomum areacomum);
+
+        void Alterar(Areacomum areacomum);
+
+        Areacomum Buscar(int idAreacomum);
+
+        void Remover(int idAreacomum);
+
+        bool Validar(Areacomum areacomum);
+
+        //IEnumerable<Areacomum> ObterPorNome(string nome);
+        //IEnumerable<Areacomum> ObterTodos();
+
+        //IEnumerable<Areacomum> ObterPorNomeOrdenadoDescendign(string nome);
+    }
+}
