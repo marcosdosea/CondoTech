7e1f1c04a6226fe91c8a853173de3a32b4de3999
Rafael Rezende <54448066+RezendeRafaelSC@users.noreply.github.com>
2021-10-29 23:48:13 -0300

    Solução para a issue #63

---
+++ Codigo/Core/DTO/CondominioDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using Core.Service;
+
+namespace Service
+{
+    class CondominioService : ICondominioService
+    {
+        private readonly CondoTechContext _context;
+
+        public CondominioService(CondoTechContext context)
+        {
+            _context = context;
+        }
+        public void AlterarCondominio(Condominio cond)
+        {
+            _context.Update(cond);
+            _context.SaveChanges();
+        }
+
+        public Condominio BuscarCondominio(int IdCondominio)
+        {
+            var _auxiliar = _context.Condominio.Find(IdCondominio);
+            return _auxiliar;
+        }
+
+        public int InserirCondominio(Condominio cond)
+        {
+            _context.Add(cond);
+            _context.SaveChanges();
+            return cond.IdCondominio;
+        }
+
+        public void RemoverCondominio(Condominio cond)
+        {
+            var _auxiliar = _context.Condominio.Find(cond.IdCondominio);
+            _context.Condominio.Remove(_auxiliar);
+            _context.SaveChanges();
+        }
+
+        public void ValidarCondominio(Condominio cond)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
