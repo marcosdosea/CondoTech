aec4f9b45d66093eeecfc1c68fd7a71f49d6020c
GuiMesc <36601466+GuiMesc@users.noreply.github.com>
2021-10-29 23:48:42 -0300

    Resolução Issue #61

---
+++ Codigo/CondoTechWEB/Models/TarefaRecorrenteModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CondoTechWEB.Models
+{
+    public class TarefaRecorrenteModel
+    {
+        [Display(Name = "Código da Tarefa")]
+        [Key]
+        [Required(ErrorMessage = "Código é obrigatório")]
+        public int IdTarefaRecorrente { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(45, MinimumLength =5, ErrorMessage = "Título de tarefa obrigatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Descrição de tarefa obrigatoria")]
+        public string Descricao { get; set; }
+
+        [Display(Name = "Data de Inicio")]
+        [DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? Data { get; set; }
+    }
+}
