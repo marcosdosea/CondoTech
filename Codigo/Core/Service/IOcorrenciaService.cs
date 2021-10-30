d3a5f10531cc5946d96914c53922daffd879db80
Pedro Antônio Santos Lima <48067351+pedroansali@users.noreply.github.com>
2021-10-29 23:04:39 -0300

    Add files via upload

---
+++ Codigo/Core/Service/IOcorrenciaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IOcorrenciaService
+    {
+        int Inserir(Ocorrencias ocorrencia);
+
+        void Validar(Ocorrencias ocorrencia);
+
+        void Alterar(Ocorrencias ocorrencia);
+
+        Ocorrencias Buscar(int IdOcorrencias);
+
+        void Remover(int IdOcorrencias);
+    }
+}
