8737a3defcb47183b8889a4cd19f76dfe473c55b
KevennyJS <kevennykeke@gmail.com>
2021-10-29 23:51:15 -0300

    resolução #65

---
+++ Codigo/Core/Service/IAvisoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAvisoService
+    {
+        int inserir(Avisos avisos);
+        Avisos buscar(int idAviso);
+        IEnumerable<Avisos> obterTodosAvisos();
+        void remover(int idAviso);
+        void alterar(Avisos avisos);
+    }
+}
