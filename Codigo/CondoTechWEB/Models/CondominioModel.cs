dd9a62eab5fdddb5ab2f18774b3d488f056ddcf8
Rafael Rezende <54448066+RezendeRafaelSC@users.noreply.github.com>
2021-10-29 23:46:26 -0300

    Solução para a issue #63

---
+++ Codigo/CondoTechWEB/Models/CondominioModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CondoTechWEB.Models
+{
+    public class CondominioModel
+    {
+        [Display(Name ="Código")]
+        [Key]
+        [Required(ErrorMessage = "Código do condomínio é obrigatório!")]
+
+        public int IdCondominio { get; set; }
+
+
+        [Display(Name ="Nome")]
+        [Required(ErrorMessage ="Campo requerido")]
+        [StringLength(50, MinimumLength =15, ErrorMessage ="Nome do condomínio é inválido!")]
+        public string Nome { get; set; }
+
+        [Display(Name ="Rua")]
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(50, MinimumLength = 15, ErrorMessage = "Nome da rua é inválida!")]
+        public string Rua { get; set; }
+
+        [Display(Name ="Bairro")]
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(50, MinimumLength = 15, ErrorMessage = "Nome do bairro é inválido!")]
+        public string Bairro { get; set; }
+
+        [Display(Name ="Numero")]
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(10, MinimumLength = 1, ErrorMessage = "Nome do número é inválido!")]
+        public int? Numero { get; set; }
+
+
+
+    }
+}
