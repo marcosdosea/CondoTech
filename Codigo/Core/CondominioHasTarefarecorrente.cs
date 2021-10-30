2d78b6a34acd174262779b09339690771f3a9259
GuiMesc <36601466+GuiMesc@users.noreply.github.com>
2021-10-29 21:49:26 -0300

    Implementando Recursos issue #56

---
+++ Codigo/Core/CondominioHasTarefarecorrente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core
+{
+    public partial class CondominioHasTarefarecorrente
+    {
+        public int CondominioIdCondominio { get; set; }
+        public int TarefaRecorrenteIdTarefaRecorrente { get; set; }
+
+        public virtual Condominio CondominioIdCondominioNavigation { get; set; }
+        public virtual Tarefarecorrente TarefaRecorrenteIdTarefaRecorrenteNavigation { get; set; }
+    }
+}
