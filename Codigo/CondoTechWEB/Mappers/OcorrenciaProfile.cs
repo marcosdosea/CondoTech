e312534b401b2a762b023501a549e425d8acca49
Pedro Antônio Santos Lima <48067351+pedroansali@users.noreply.github.com>
2021-10-29 23:06:27 -0300

    Add files via upload

---
+++ Codigo/CondoTechWEB/Mappers/OcorrenciaProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Models;
+using Core;
+
+namespace Mappers
+{
+    public class OcorrenciaProfile : Profile
+    {
+        public OcorrenciaProfile()
+        {
+            CreateMap<OcorrenciaModel, Ocorrencias>().ReverseMap();
+        }
+    }
+}
