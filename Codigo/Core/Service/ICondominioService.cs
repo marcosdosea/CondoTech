6a7d8935f8790e5b513f80b1e98775bc66f0f517
Rafael Rezende <54448066+RezendeRafaelSC@users.noreply.github.com>
2021-10-29 23:47:09 -0300

    Solução para a issue #63

---
+++ Codigo/Core/Service/ICondominioService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface ICondominioService
+    {
+        int InserirCondominio(Condominio cond);
+        void ValidarCondominio(Condominio cond);
+        Condominio BuscarCondominio(Condominio cond);
+        void RemoverCondominio(Condominio cond);
+        void AlterarCondominio(Condominio cond);
+        Condominio AlterarCondominio(int id);
+    }
+}
