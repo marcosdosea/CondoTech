ec0708f24f7543c537be7f37fb1d3d04c2110343
Rafael Rezende <54448066+RezendeRafaelSC@users.noreply.github.com>
2021-11-17 23:16:59 -0300

    Resolução para a issue #78

--- Codigo/CondoTechWEB/Models/CondominioModel.cs
+++ Codigo/CondoTechWEB/Models/CondominioModel.cs
@@ -11,31 +11,38 @@ namespace CondoTechWEB.Models
         [Display(Name ="Código")]
         [Key]
         [Required(ErrorMessage = "Código do condomínio é obrigatório!")]
-
         public int IdCondominio { get; set; }
 
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(18, MinimumLength = 18, ErrorMessage = "CNPJ inválido!")]
+        public string Cnpj { get; set; }
 
-        [Display(Name ="Nome")]
-        [Required(ErrorMessage ="Campo requerido")]
-        [StringLength(50, MinimumLength =15, ErrorMessage ="Nome do condomínio é inválido!")]
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 15, ErrorMessage ="Nome do condomínio é inválido!")]
         public string Nome { get; set; }
 
-        [Display(Name ="Rua")]
         [Required(ErrorMessage = "Campo requerido")]
-        [StringLength(50, MinimumLength = 15, ErrorMessage = "Nome da rua é inválida!")]
+        [StringLength(14, MinimumLength = 12, ErrorMessage = "Telefone inválido!")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 15, ErrorMessage = "Email é inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 15, ErrorMessage = "Nome da rua é inválida!")]
         public string Rua { get; set; }
 
-        [Display(Name ="Bairro")]
         [Required(ErrorMessage = "Campo requerido")]
-        [StringLength(50, MinimumLength = 15, ErrorMessage = "Nome do bairro é inválido!")]
+        [StringLength(45, MinimumLength = 15, ErrorMessage = "Nome do bairro é inválido!")]
         public string Bairro { get; set; }
 
-        [Display(Name ="Numero")]
         [Required(ErrorMessage = "Campo requerido")]
-        [StringLength(10, MinimumLength = 1, ErrorMessage = "Nome do número é inválido!")]
         public int? Numero { get; set; }
-
-
+        [Required(ErrorMessage = "Campo requerido")]
+        [StringLength(45, MinimumLength = 15, ErrorMessage = "O CEP é inválido!")]
+        public string Cep { get; set; }
 
     }
 }
