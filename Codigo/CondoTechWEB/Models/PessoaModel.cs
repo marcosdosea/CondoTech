cb69b0ca4bfb1742c9cd1e1ea8ec35bf4bed4e0a
GuiMesc <36601466+GuiMesc@users.noreply.github.com>
2021-11-17 22:59:11 -0300

    Model Issue #75

--- Codigo/CondoTechWEB/Models/PessoaModel.cs
+++ Codigo/CondoTechWEB/Models/PessoaModel.cs
@@ -17,7 +17,26 @@ namespace CondoTechWEB.Models
         [StringLength(45, MinimumLength = 5, ErrorMessage = "Nome da pessoa é obrigatorio")]
         public string Nome { get; set; }
 
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(15, MinimumLength = 11, ErrorMessage = "Número do CPF é obrigatorio")]
+        public string Cpf { get; set; }
 
-       
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(14, MinimumLength = 10, ErrorMessage = "Número do telefone é obrigatorio")]
+        public string Telefone { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage = "Email da pessoa é obrigatorio")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [StringLength(45, MinimumLength = 8, ErrorMessage = "Senha é obrigatoria")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+
+        [Required(ErrorMessage = "Campo Requerido")]
+        [RegularExpression("Ativo|Bloqueado|Excluido", ErrorMessage ="Informe um status entre: Ativo,Bloqueado, Excluido")]
+        public string Status { get; set; }
+
     }
 }
