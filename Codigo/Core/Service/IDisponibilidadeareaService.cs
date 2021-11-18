4c515e5172817854eaa2130265ca966319658dc7
raul152 <48693258+raul152@users.noreply.github.com>
2021-11-17 21:12:31 -0300

    Add files via upload

---
+++ Codigo/Core/Service/IDisponibilidadeareaService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IDisponibilidadeareaService
+    {
+        int Insert(Disponibilidadearea disponibilidadearea);
+
+        void Update(Disponibilidadearea disponibilidadearea);
+
+        Disponibilidadearea Get(int IdDisponibilidadeArea);
+
+        void Delete(int IdDisponibilidadeArea);
+
+        bool Validar(Disponibilidadearea disponibilidadearea);
+
+        IEnumerable<Disponibilidadearea> GetByDiaSemana(string DiaSemana);
+        IEnumerable<Disponibilidadearea> GetAll();
+        IQueryable<Disponibilidadearea> GetQuery();
+    }
+}
