814197ae629a1d4a4b6a990478befe9674208278
raul152 <48693258+raul152@users.noreply.github.com>
2021-11-17 22:49:34 -0300

    Add files via upload

--- Codigo/CondoTechWEB/Models/AreacomumModel.cs
+++ Codigo/CondoTechWEB/Models/AreacomumModel.cs
@@ -8,11 +8,21 @@ namespace CondoTechWEB.Models
 {
     public class AreacomumModel
     {
-        [Display(Name ="Código")]
+        [Display(Name ="Código da área comum")]
         [Key]
         [Required(ErrorMessage ="Código da área comum é obrigatório")]
 
-        public int IdAreacomum { get; set; }
+        public int IdAreaComum { get; set; }
+
+        [Display(Name ="Código do condominio")]
+        [Required(ErrorMessage ="Código do condominio é obrigatório")]
+
+        public int IdCondominio { get; set; }
+
+        [Required(ErrorMessage ="O nome da área é necessário")]
+        [StringLength(45, MinimumLength = 5, ErrorMessage ="O nome da área deve ter entre 5 a 45 caracteres")]
+
+        public string Nome { get; set; }
 
         [Display(Name ="Descrição")]
         [Required(ErrorMessage="A descrição da área é necessária")]
@@ -20,14 +30,9 @@ namespace CondoTechWEB.Models
 
         public string Descricao { get; set; }
 
-        [Display(Name = "Dias")]
-        [Required(ErrorMessage ="Os dias das áreas são necessários")]
-        [StringLength(45, MinimumLength = 5, ErrorMessage ="Os dias devem ter entre 5 e 45 caracteres")]
-        //[DataType(DataType.Date, ErrorMessage = "Data válida requerida")]
-        //[DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        
 
-        //public DateTime? Dias { get; set; }
-        public string Dias { get; set; }
+        
 
 
 
