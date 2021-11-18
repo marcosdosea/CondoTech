1b2f5e46c4ac1ce5d66b456b5b480fe17f03afbe
Rafael Rezende <54448066+RezendeRafaelSC@users.noreply.github.com>
2021-11-17 23:13:09 -0300

    Resolução para a Issue #78

--- Codigo/CondoTechWEB/Controllers/CondominioController.cs
+++ Codigo/CondoTechWEB/Controllers/CondominioController.cs
@@ -10,39 +10,38 @@ namespace CondoTechWEB.Controllers
 {
     public class CondominioController : Controller
     {
-        ICondominioService _condominioService;
-        IMapper _mapper;
+        private readonly ICondominioService _condominioService;
+        private readonly IMapper _mapper;
 
     public CondominioController(ICondominioService condominioService, IMapper mapper)
         {
             _condominioService = condominioService;
             _mapper = mapper;
         }
-
-    //comentei para testes dia 09/11/2021
-     /*public ActionResult Index()
+     // GET: CondominioController
+        public ActionResult Index()
         {
-            var listaCondominio = _condominioService.ObterTodos();
-            var listaCondominiosModel = _mapper.Map<List<CondominioModel>(listaCondominio);
-            return View(CondominioModel);
+            var listaCondominio = _condominioService.GetAll();
+            var listaCondominiosModel = _mapper.Map<List<CondominioModel>>(listaCondominio);
+            return View(listaCondominiosModel);
 
-        }*/
-    public ActionResult Details(int id)
+        }
+    // GET: TarefaRecorrenteController/Details/
+        public ActionResult Details(int id)
     {
         Condominio condominio = _condominioService.Get(id);
         CondominioModel condominioModel = _mapper.Map<CondominioModel>(condominio);
         return View(condominioModel);
     }
 
-     public ActionResult Create()
+     //GET: CondominioController/Create
+        public ActionResult Create()
         {
             return View();
         }
-
-
+     // POST: CondominioController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-
         public ActionResult Create (CondominioModel condominioModel)
         {
             if (ModelState.IsValid)
@@ -53,22 +52,31 @@ namespace CondoTechWEB.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        
+        public ActionResult Edit (int id)
+        {
+            Condominio condominio = _condominioService.Get(id);
+            CondominioModel condominioModel = _mapper.Map<CondominioModel>(condominio);
+            return View(condominioModel);
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
+        // GET: CondominioController/Edit/
 
         public ActionResult Edit(int id, CondominioModel condominioModel)
         {
+
             if (ModelState.IsValid)
             {
+
                 var condominio = _mapper.Map<Condominio>(condominioModel);
                 _condominioService.Update(condominio);
             }
             return RedirectToAction(nameof(Index));
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-
+        // GET: CondomonioController/Delete
         public ActionResult Delete(int id)
         {
             Condominio condominio = _condominioService.Get(id);
